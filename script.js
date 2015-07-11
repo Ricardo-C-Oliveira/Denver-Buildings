79870e46655541a1fb1dc173b7179c1e411d790f
Ricardo Oliveira <oliveira.ricardo@hotmail.com>
2015-07-11 00:22:25 -0600

    add base tilelayer

---
+++ script.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiZGFya3ZlbmdlcnMiLCJhIjoidFQyQi15ayJ9.0ImOfSMr3xjHj9W1Ilbo1g';
+// Create a map in the div #map
+var map = L.mapbox.map('map', 'darkvengers.mmbaoed5');
