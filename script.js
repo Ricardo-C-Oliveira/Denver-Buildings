0685b3ff6b47def2d43db4bcfe16ae3c572bf9ea
Ricardo Oliveira <oliveira.ricardo@hotmail.com>
2015-07-12 00:26:02 -0600

    add sidebar

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZGFya3ZlbmdlcnMiLCJhIjoidFQyQi15ayJ9.0ImOfSMr3xjHj9W1Ilbo1g';
+//set maxBounds
+var southWest = L.latLng(39.590034, -105.159291),
+  northEast = L.latLng(39.932998, -104.57759),
+  bounds = L.latLngBounds(southWest, northEast);
 // Create a map in the div #map
-var map = L.mapbox.map('map', 'darkvengers.mmbaoed5');
+var map = L.map('map', {
+  'zoomControl': false,
+  'maxBounds': bounds,
+  'maxZoom': 16,
+  'minZoom': 13
+}).setView([39.745335, -104.993127], 13);
+
+var layer = L.mapbox.tileLayer('darkvengers.fadeb801').addTo(map);
+
+//collapse and show sidebar
+$('#sidebar').slideReveal({
+  trigger: $("#tabIcon"),
+  width: 300,
+  //push: false,
+});
