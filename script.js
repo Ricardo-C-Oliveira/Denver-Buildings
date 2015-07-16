af17d81252bfc7adf8662421e1cf2aba9da597a5
Ricardo Oliveira <oliveira.ricardo@hotmail.com>
2015-07-15 22:40:35 -0600

    add right side bar

--- script.js
+++ script.js
@@ -15,33 +15,38 @@ var layer = L.mapbox.tileLayer('darkvengers.fadeb801').addTo(map);
 
 //custom geocoding control
 function showMap(err, data) {
-    // The geocoder can return an area, like a city, or a
-    // point, like an address. Here we handle both cases,
-    // by fitting the map bounds to an area or zooming to a point.
-    if (!map) {
-        map = L.mapbox.map('map', 'examples.map-h67hf2ic');
-    }
+  // The geocoder can return an area, like a city, or a
+  // point, like an address. Here we handle both cases,
+  // by fitting the map bounds to an area or zooming to a point.
+  if (!map) {
+    map = L.mapbox.map('map', 'examples.map-h67hf2ic');
+  }
 
-    if (data.lbounds) {
-        map.fitBounds(data.lbounds);
-    } else if (data.latlng) {
-        map.setView([data.latlng[0], data.latlng[1]], 13);
-    }
+  if (data.lbounds) {
+    map.fitBounds(data.lbounds);
+  } else if (data.latlng) {
+    map.setView([data.latlng[0], data.latlng[1]], 13);
+  }
 }
 
 
 function geocodeThis() {
-    var text = document.getElementById('search').value;
-    if (text.length >= 5) {
-        geocoder.query(text, showMap);
-    }
+  var text = document.getElementById('search').value;
+  if (text.length >= 5) {
+    geocoder.query(text, showMap);
+  }
 }
 
+  //collapse and show sidebar
+  $('#sidebarLeft').slideReveal({
+    trigger: $("#toggleLeft"),
+    width: 300,
+    push: false,
+  });
 
-
-//collapse and show sidebar
-$('#sidebar').slideReveal({
-  trigger: $("#tabIcon"),
-  width: 300,
-  //push: false,
-});
+  $('#sidebarRight').slideReveal({
+    trigger: $("#toggleRight"),
+    width: 250,
+    push: false,
+    position: "right",
+  });
